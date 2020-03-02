3a5a9a4a33b61bdc868dc6cea687932dd2a77f8e
Phillip Burt <pab37@students.uwf.edu>
2020-03-02 12:59:51 -0600

    hunt_1 button made, in the process of creating the AR fragment and connect the too

--- app/src/main/java/com/example/argohunt/ArgoHuntFragment.java
+++ app/src/main/java/com/example/argohunt/ArgoHuntFragment.java
@@ -2,14 +2,20 @@ package com.example.argohunt;
 
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
 import com.example.argohunt.models.ArgoHunt;
 
 public class ArgoHuntFragment extends Fragment
 {
     private ArgoHunt mArgoHunt;
+    private Button mHunt_1;
 
     public static ArgoHuntFragment newInstance()
     {
@@ -20,5 +26,16 @@ public class ArgoHuntFragment extends Fragment
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
+    {
+        View v = inflater.inflate(R.layout.fragment_hunt, container,false);
+
+        mHunt_1 = v.findViewById(R.id.hunt_1);
+
+        return v;
     }
 }
